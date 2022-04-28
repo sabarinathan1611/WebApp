d7eeb52d58b94109e65552b3abe7cf69b63b2999
Harish <sabarinathan.project@gmail.com>
2022-04-28 12:03:27 +0530

    User Login and  signUP created with OTP verify

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from distutils.log import debug
-from templates import create_app
-import os
+from settings import create_app
 app = create_app()
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
+    
+    
+    
+    
+
+    
--- settings/models.py
+++ settings/models.py
@@ -1,24 +1,24 @@
 from datetime import timezone
-from email.policy import default
-from enum import unique
-from sqlalchemy import PrimaryKeyConstraint
 from .import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class Note(db.Model):
-     id = db.Column(db.Integer,Primary_key=True)
+     id = db.Column(db.Integer,primary_key=True)
      data =db.Column(db.String(10000))
      date= db.Column(db.DateTime(timezone=True),default=func.now())
-     user_id =db.Column(db.Integer,db.models.ForeignKey('user.id'))
+     user_id =db.Column(db.Integer,db.ForeignKey('user.id'))
       
 
 class User(db.Model,UserMixin):
-    id = db.Column(db.Integer,Primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(150), unique =True)
     password=db.Column(db.String(150))
-    user_name=db.Column(db.String(150), unique=True)
+    user_name=db.Column(db.String(150), unique =True)
     notes = db.relationship('Note')
+    def __repr__(self) :
+        return '<Name %r>' % self.id
+   
     
     
 
